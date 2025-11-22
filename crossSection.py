6361f6c929626e511f2d8ee12a2b3b05bcbefecb
DorianSpilette <45554495+DorianSpilette@users.noreply.github.com>
2025-11-22 15:08:05 +0100

    feat(molar): ajout des valeurs pour halflife,crossectione t molarmass + README

--- crossSection.py
+++ crossSection.py
@@ -1,5 +1,6 @@
+"""
 def crossSection(X, Transfo, E_neutron):
-    """
+    
     Computes the cross section for a specific energy level
     ENDF database: https://www-nds.iaea.org/exfor/endf.htm
 
@@ -15,7 +16,7 @@ def crossSection(X, Transfo, E_neutron):
         the value should be in the range [1e-5; 2e7] [eV]
     :return: array, list
         values of the cross section in [barn] corresponding to the input parameters
-    """
+    
 
     # Your work : here
 
@@ -31,3 +32,122 @@ def crossSection(X, Transfo, E_neutron):
 
 # Try the function:
 # cs = crossSection(X='Pu240', Transfo='Fission', E_neutron=np.logspace(-5,6,10000).tolist())
+"""
+
+# crossSection.py
+
+import numpy as np
+
+
+def crossSection(X, Transfo, E_neutron):
+    """
+    Computes the cross section for a specific energy level
+    ENDF database: https://www-nds.iaea.org/exfor/endf.htm
+
+    ----------------
+    :param X: string
+        nuclide or nucleon (for a neutron), follows the atomic notation of the element,
+        e.g.: for Uranium-235, X = 'U235'
+    :param Transfo: string
+        name of the considered transformation, should be one of:
+        ['Fission', 'Capture']
+    :param E_neutron: array-like
+        energy(ies) of the incident neutron in [eV]
+        expected range [1e-5; 2e7] [eV]
+    :return: numpy array of same shape as E_neutron
+        cross section in [barn]
+    """
+
+    E = np.array(E_neutron, dtype=float)
+
+    # output
+    sigma = np.zeros_like(E)
+
+    # define energy groups
+    E_th = 1.0     # eV
+    E_fast = 1e6   # eV
+
+    # simple values (order of magnitude; enough for the project)
+    # thermal values
+    sig = {
+        ("U235", "Fission", "th"): 580.0,
+        ("U235", "Capture", "th"): 100.0,
+
+        ("U238", "Fission", "th"): 0.02,
+        ("U238", "Capture", "th"): 2.7,
+
+        ("Pu239", "Fission", "th"): 750.0,
+        ("Pu239", "Capture", "th"): 270.0,
+
+        ("Pu240", "Fission", "th"): 0.5,
+        ("Pu240", "Capture", "th"): 290.0,
+
+        ("Xe135", "Capture", "th"): 2.5e6,  # huge poison
+    }
+
+    # fast values (smaller fission XS, small captures)
+    sig_fast = {
+        ("U235", "Fission"): 1.0,
+        ("U235", "Capture"): 1.0,
+
+        ("U238", "Fission"): 0.3,
+        ("U238", "Capture"): 0.5,
+
+        ("Pu239", "Fission"): 1.5,
+        ("Pu239", "Capture"): 1.0,
+
+        ("Pu240", "Fission"): 0.1,
+        ("Pu240", "Capture"): 1.0,
+
+        ("Xe135", "Capture"): 1e3,  # much smaller than thermal but still sizable
+    }
+
+    # masks
+    mask_th = E <= E_th
+    mask_fast = E >= E_fast
+    mask_mid = (~mask_th) & (~mask_fast)
+
+    key_th = (X, Transfo, "th")
+    key_fast = (X, Transfo)
+
+    if key_th not in sig and key_fast not in sig_fast:
+        print("\n WARNING : No cross section data for", X, "/", Transfo)
+        return sigma
+
+    # thermal region
+    if key_th in sig:
+        sigma[mask_th] = sig[key_th]
+
+    # fast region
+    if key_fast in sig_fast:
+        sigma[mask_fast] = sig_fast[key_fast]
+
+    # intermediate region: log-linear interpolation between thermal and fast values
+    if np.any(mask_mid):
+        # if one of the endpoints is missing, just use the one we have
+        sigma_mid = np.zeros_like(E[mask_mid])
+
+        if key_th in sig and key_fast in sig_fast:
+            s_th = sig[key_th]
+            s_fast = sig_fast[key_fast]
+            logE = np.log(E[mask_mid])
+            logE_th = np.log(E_th)
+            logE_fast = np.log(E_fast)
+            w = (logE - logE_th) / (logE_fast - logE_th)
+            sigma_mid = s_th + w * (s_fast - s_th)
+        elif key_th in sig:
+            sigma_mid[:] = sig[key_th]
+        elif key_fast in sig_fast:
+            sigma_mid[:] = sig_fast[key_fast]
+
+        sigma[mask_mid] = sigma_mid
+
+    return sigma
+
+
+# Simple test
+if __name__ == "__main__":
+    E_test = np.logspace(-5, 7, 5)
+    cs = crossSection("U235", "Fission", E_test)
+    print("E [eV] :", E_test)
+    print("sigma  :", cs, "barn")
--- halfLife.py
+++ halfLife.py
@@ -1,5 +1,6 @@
+""""
 def halfLife(X, Transfo):
-    """
+
     Return the half life of a nuclide or nucleon for a specific transformation
     Data available on http://wwwndc.jaea.go.jp/NuC/ (more precisely: http://wwwndc.jaea.go.jp/CN14/index.html)
 
@@ -11,7 +12,7 @@ def halfLife(X, Transfo):
         name of the considered transformation, should be one of the following in the list
         ['Alpha', 'BetaMinus', 'BetaPlus', 'Gamma']
     :return: the half life of X for the transformation Transfo in seconds
-    """
+
 
     # Your work : here
 
@@ -28,6 +29,73 @@ def halfLife(X, Transfo):
     # hl = 0.
 
     return hl
-
+"""
 # Try your function
-# hl = halfLife(X='Pa233', Transfo='BetaMinus')
+# hl = halfLife(X='Pa233', Transfo='BetaMinus')# halfLife.py
+############################################################################### database of half-lives in seconds
+import math
+
+# half-lives database in seconds
+# (only what we really need; others are approximations)
+HL_DB = {
+    # Thorium
+    ("Th232", "Alpha"): 4.4e17,      # ~1.4e10 yr
+    ("Th233", "BetaMinus"): 5.0e3,   # ~1.4 h
+
+    # Protactinium
+    ("Pa233", "BetaMinus"): 9.9e4,   # ~27.4 h
+
+    # Uranium (mostly irrelevant on short times, but put something)
+    ("U233", "Alpha"): 5.2e12,       # ~1.6e5 yr
+    ("U235", "Alpha"): 2.2e16,       # ~7e8 yr
+    ("U236", "Alpha"): 7.4e14,       # ~2.3e7 yr
+    ("U237", "BetaMinus"): 2.2e3,    # ~38 min
+    ("U238", "Alpha"): 1.4e17,       # ~4.5e9 yr
+    ("U239", "BetaMinus"): 2.3e2,    # ~23 min
+
+    # Neptunium
+    ("Np239", "BetaMinus"): 8.4e4,   # ~2.35 d
+
+    # Plutonium
+    ("Pu239", "Alpha"): 7.6e15,      # ~2.4e4 yr
+    ("Pu240", "Alpha"): 2.0e15,      # ~6.6e3 yr
+
+    # Xenon-135
+    ("Xe135", "BetaMinus"): 3.29e4,  # ~9.14 h
+
+    # Fictitious fission product (delayed neutrons source)
+    ("FP", "BetaMinus"): 1.0,        # given: T1/2 ~ 1 s
+}
+
+
+def halfLife(X, Transfo):
+    """
+    Return the half life of a nuclide or nucleon for a specific transformation
+    Data available on http://wwwndc.jaea.go.jp/NuC/
+
+    ----------------
+    :param X: string
+        nuclide or nucleon (for a neutron), that follows the atomic notation of the element,
+        i.e.: for Uranium-235, X = 'U235'
+    :param Transfo: string
+        name of the considered transformation, should be one of:
+        ['Alpha', 'BetaMinus', 'BetaPlus', 'Gamma']
+    :return: the half life of X for the transformation Transfo in seconds
+    """
+
+    if (X, Transfo) in HL_DB:
+        hl = HL_DB[(X, Transfo)]
+    else:
+        print("\n WARNING : No half-life in database for (", X, ",", Transfo, ")")
+        hl = 0.0
+
+    return hl
+
+
+# Small test
+if __name__ == "__main__":
+    hl_fp = halfLife("FP", "BetaMinus")
+    hl_xe = halfLife("Xe135", "BetaMinus")
+    print("T1/2(FP)   =", hl_fp, "s")
+    print("T1/2(Xe135) =", hl_xe, "s")
+
